23e7757d1cb80edf940c229cf55c163086330255
ferrandroid <ferran.html@gmail.com>
2019-05-22 15:57:27 +0200

    Funciones de primer nivel

--- sumaTodos.py
+++ sumaTodos.py
@@ -7,4 +7,11 @@ def sumaTodos(limitTo):
 
 print (sumaTodos(100))
 
-    
+def sumaTodosLosCuadrados(limitTo):
+    resultado = 0
+    for i in range(0, limitTo+1):
+        resultado += i*i
+        
+    return resultado
+
+print (sumaTodosLosCuadrados(2))
